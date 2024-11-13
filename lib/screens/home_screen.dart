bd5957dd82d23f72355175dbfa2838b0bc5543de
osama <osamakhaliluni@gmail.com>
2024-11-13 04:38:08 +0300

    login screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -31,7 +29,6 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future<void> _refreshProducts() async {
-    print("Refreshing");
     await ProductsCubit.get(context).getProducts();
   }
 
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/core/style/colors.dart';
+import 'package:frontend/utils/size_config.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: primaryColor,
+        appBar: AppBar(
+          backgroundColor: primaryColor,
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              right: 0,
+              left: 0,
+              child: SizedBox(
+                height: SizeConfig.screenHeight / 4,
+                child: const Center(
+                  child: Text("Real Estate"),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                constraints: BoxConstraints(
+                  minHeight: SizeConfig.screenHeight / 3 * 2,
+                ),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: lightColor,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text("Login"),
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 50,
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Icon(
+                                    Icons.person,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      floatingLabelBehavior:
+                                          FloatingLabelBehavior.never,
+                                      labelText: "Username",
+                                      labelStyle: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w100,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 1,
+                            decoration: const BoxDecoration(
+                              color: Colors.black38,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 50,
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Icon(
+                                    Icons.lock_open,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: TextField(
+                                    obscureText: true,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      floatingLabelBehavior:
+                                          FloatingLabelBehavior.never,
+                                      labelText: "Password",
+                                      labelStyle: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w100,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          onTap: () {},
+                          child: const Text("Forgot Password?"),
+                        ),
+                        SizedBox(
+                          width: SizeConfig.screenWidth / 3,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text("Login"),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
